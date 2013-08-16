3bc58c21e22ab2b747aa2ee5a97911b0ab6fa249
Huey Petersen <eyston@gmail.com>
2013-08-16 15:44:08 -0400

    massive clean up -- atomic commits ftw!

--- js/evemaps.js
+++ js/evemaps.js
@@ -44,7 +44,9 @@ angular.module('evemaps').factory('MapsService', ['$window', function($window) {
 
 angular.module('evemaps').directive('evemap', ['$window', 'MapsService', function($window, mapsService) {
 
-    function mapBounds(systems) {
+    // TODO: figure out which of these functions belong to the SVG renderer and which belong to the data structure
+
+    function _mapBounds(systems) {
         var minX = _(systems).min(function(s) { return s.x; }).x;
         var maxX = _(systems).max(function(s) { return s.x; }).x;
 
@@ -59,6 +61,222 @@ angular.module('evemaps').directive('evemap', ['$window', 'MapsService', functio
         };
     };
 
+    function translationForPoint(map, scale, point) {
+        return [map.screen.w / 2 - map.scale.x(point.x) * scale, map.screen.h / 2 - map.scale.z(point.z) * scale];
+    };
+
+    function mapCenter(map) {
+        return { x: map.bounds.x + map.bounds.w / 2, z: map.bounds.y + map.bounds.h / 2 };
+    };
+
+    function createMap(svg, bounds) {
+
+        var size = 500,
+            x = d3.scale.linear()
+                .domain([0, bounds.w])
+                .range([0, size]),
+            z = d3.scale.linear()
+                .domain([0, bounds.w])
+                .range([0, -size]);
+
+        var zoom = d3.behavior.zoom()
+            .scaleExtent([1, 128]);
+
+        svg.call(zoom);
+
+        var map = {
+            element: svg.append('g'),
+            screen: {
+                h: $window.innerHeight,
+                w: $window.innerWidth
+            },
+            bounds: bounds,
+            scale: {
+                x: x,
+                z: z
+            },
+            zoom: zoom,
+            colors: d3.scale.category10()
+        };
+
+        // create observables
+        map.zooms = new Rx.Subject();
+        zoom.on("zoom", function() { map.zooms.onNext(map); });
+
+        map.zooms.subscribe(transformMap);
+
+        // meh ...
+        map.zooms
+            .select(function(map) {
+                return [map, snapToPowerOfTwo(truncate(map.zoom.scale()))];
+            })
+            .distinctUntilChanged(function(d) {
+                return d[1];
+            })
+            .subscribe(function(d) { scaleMap(d[0], d[1]); });
+
+        return map;
+    };
+
+    function truncate(value) {
+        return value | 0;
+    };
+
+    function snapToPowerOfTwo(value) {
+        return Math.pow(2, ((Math.log(value)/Math.log(2)) | 0)) | 0;
+    };
+
+
+    function zoomMap(map, scale, translate) {
+        map.zoom.scale(scale);
+        map.zoom.translate(translate);
+
+        map.zooms.onNext(map);
+    };
+
+    function transformMap(map) {
+        map.element.attr('transform', 'translate(' + map.zoom.translate()[0] + ', ' + map.zoom.translate()[1] + ')scale(' + map.zoom.scale() + ')');
+    };
+
+    function scaleMap(map, scale) {
+        map.element.attr('class', 'zoom' + scale);
+    };
+
+    function drawJumps(map, jumps) {
+        map.element.append('g').selectAll('line')
+            .data(jumps)
+            .enter()
+                .append('line')
+                .attr('class', 'solar')
+                .attr('x1', function(d) { return map.scale.x(d.to.x); })
+                .attr('y1', function(d) { return map.scale.z(d.to.z); })
+                .attr('x2', function(d) { return map.scale.x(d.from.x); })
+                .attr('y2', function(d) { return map.scale.z(d.from.z); });
+    };
+
+    function drawRegionJumps(map, regionJumps) {
+        map.element.append('g').selectAll('line')
+            .data(regionJumps)
+            .enter()
+                .append('line')
+                .attr('class', 'region')
+                .attr('x1', function(d) { return map.scale.x(d.to.x); })
+                .attr('y1', function(d) { return map.scale.z(d.to.z); })
+                .attr('x2', function(d) { return map.scale.x(d.from.x); })
+                .attr('y2', function(d) { return map.scale.z(d.from.z); });
+    };
+
+    function createSystemsRTree(systems) {
+        var tree = new RTree();
+
+        _(systems).each(function(s) {
+            var length = 4000000000000;
+
+            var minX = s.x - length;
+            var maxX = s.x + length;
+            var minZ = s.z - length;
+            var maxZ = s.z + length;
+
+            tree.insert({
+                x: minX,
+                y: minZ,
+                w: maxX - minX,
+                h: maxZ - minZ
+            }, s);
+        });
+
+        return tree;
+    };
+
+    function viewableBounds(map) {
+        return {
+            x: map.scale.x.invert((0 - map.zoom.translate()[0]) / map.zoom.scale()),
+            y: map.scale.z.invert((map.screen.h - map.zoom.translate()[1]) / map.zoom.scale()),
+            w: (map.scale.x.invert(map.screen.w) - map.scale.x.invert(0)) / map.zoom.scale(),
+            h: (map.scale.z.invert(0) - map.scale.z.invert(map.screen.h)) / map.zoom.scale()
+        };
+    }
+
+    function drawSystems(map, systems) {
+        var ss = map.element.selectAll('circle')
+            .data(systems, function(s) {
+                return s.id;
+            });
+
+        ss.enter()
+            .append('circle')
+            .attr('cx', function(d) { return map.scale.x(d.x); })
+            .attr('cy', function(d) { return map.scale.z(d.z); })
+            .attr('r', .15)
+            .style('stroke-width', '0.05')
+            .style('stroke', function(d) { return map.colors(d.regionId); })
+            .style('fill', '#FFF');
+
+        ss.exit()
+            .remove();
+    };
+
+    function clearSystems(map) {
+        map.element.selectAll('circle').remove();
+    };
+
+    function drawSystemLabels(map, systems) {
+        var ss = map.element.selectAll('text')
+            .data(systems, function(s) {
+                return s.id;
+            });
+
+        ss.enter()
+            .append('text')
+            .attr('x', function(d) { return map.scale.x(d.x); })
+            .attr('y', function(d) { return map.scale.z(d.z); })
+            .attr('dy', '0.175em')
+            .attr('dx', .5)
+            .style({
+                'font-family': 'helvetica, arial',
+                'font-size': '0.5px'
+            })
+            .text(function(d) { return d.name; });
+
+        ss.exit()
+            .remove();
+    };
+
+    function clearSystemLabels(map) {
+        map.element.selectAll('text').remove();
+    };
+
+    function zoomSystem(map, system) {
+
+        // TOOD:  break this out into a zoomBox thang
+
+        // determines how big the screen should be
+        var height = 30000000000000000;
+        var width = height * (map.screen.w / map.screen.h);
+
+        var scale = map.scale.x.invert(map.screen.w) / width;
+        var translation = translationForPoint(map, scale, system);
+
+        transitionMap(map, scale, translation);
+    };
+
+    function transitionMap(map, scale, translation) {
+        d3.transition().duration(2000).tween("zoom", function() {
+
+            var ix = d3.interpolate(map.zoom.translate()[0], translation[0]),
+                iy = d3.interpolate(map.zoom.translate()[1], translation[1])
+                is = d3.interpolate(map.zoom.scale(), scale);
+
+            return function(t) {
+                map.zoom.translate([ix(t), iy(t)]);
+                map.zoom.scale(is(t));
+
+                map.zooms.onNext(map);
+            };
+        });
+    };
+
+
     return {
         restrict: 'E',
         replace: true,
@@ -68,195 +286,48 @@ angular.module('evemaps').directive('evemap', ['$window', 'MapsService', functio
         },
 
         link: function(scope, element, attrs) {
-            var svg = d3.select(element[0]);
-            var map = svg.append('g');
-
-            var view = {
-                // screen resolution in pixels
-                screen: {
-                    h: $window.innerHeight,
-                    w: $window.innerWidth
-                },
-                // map bounds in map coordinates
-                map: mapBounds(mapsService.systems)
+            var data = {
+                systems: mapsService.systems,
+                jumps: mapsService.jumps,
+                regionJumps: mapsService.regionJumps
             };
 
-            var size = 500;
-            var x = d3.scale.linear()
-                .domain([0, view.map.w])
-                .range([0, size]);
-
-            var z = d3.scale.linear()
-                .domain([0, view.map.w])
-                .range([0, -size]);
-
-            var regionJumps = map.append('g');
-
-            regionJumps.selectAll('line')
-                .data(mapsService.regionJumps)
-                .enter()
-                    .append('line')
-                    .attr('class', 'region')
-                    .attr('x1', function(d) { return x(d.to.x); })
-                    .attr('y1', function(d) { return z(d.to.z); })
-                    .attr('x2', function(d) { return x(d.from.x); })
-                    .attr('y2', function(d) { return z(d.from.z); });
-
-            var jumps = map.append('g');
-
-            jumps.selectAll('line')
-                .data(mapsService.jumps)
-                .enter()
-                    .append('line')
-                    .attr('class', 'solar')
-                    .attr('x1', function(d) { return x(d.to.x); })
-                    .attr('y1', function(d) { return z(d.to.z); })
-                    .attr('x2', function(d) { return x(d.from.x); })
-                    .attr('y2', function(d) { return z(d.from.z); });
-
-            var systemTree = new RTree();
-            _(mapsService.systems).each(function(s) {
-                var length = 4000000000000;
-
-                var minX = s.x - length;
-                var maxX = s.x + length;
-                var minZ = s.z - length;
-                var maxZ = s.z + length;
-
-                systemTree.insert({
-                    x: minX,
-                    y: minZ,
-                    w: maxX - minX,
-                    h: maxZ - minZ
-                }, s);
-            });
-
-            function centerOnPoint(view, point) {
-                return [
-                    view.screen.w / 2 - x(point.x),
-                    z(point.y) + view.screen.h / 2
-                ];
-            };
+            var map = createMap(d3.select(element[0]), _mapBounds(data.systems));
+            drawJumps(map, data.jumps);
+            drawRegionJumps(map, data.regionJumps);
 
             // we want to translate so map is in middle
-            var translate = centerOnPoint(view, { x: view.map.x + view.map.w / 2, y: view.map.y + view.map.h / 2 });
+            var scale = 2,
+                centerTranslation = translationForPoint(map, scale, mapCenter(map));
 
-            var zoom = d3.behavior.zoom()
-                .scaleExtent([1, 128])
-                .translate(translate)
-                .scale(2)
-                .on("zoom", zoomed);
+            zoomMap(map, scale, centerTranslation);
 
-            svg.call(zoom);
+            var systemsTree = createSystemsRTree(data.systems);
 
-            var scale;
-            var colors = d3.scale.category10();
+            map.zooms.subscribe(function(map) {
+                var bounds = viewableBounds(map);
 
-            function zoomed() {
-                map.attr('transform', 'translate(' + zoom.translate()[0] + ', ' + zoom.translate()[1] + ')scale(' + zoom.scale() + ')');
+                // TODO: no point doing this when scale < 8 ...
+                var systems = systemsTree.search(bounds);
 
-                // clean up below here!
-
-                var newScale = zoom.scale() | 0;
-
-                if(scale !== newScale)
-                {
-                    scale = newScale;
-                    d3.select('svg g').attr('class', 'zoom' + Math.pow(2, ((Math.log(scale)/Math.log(2)) | 0)));
-                }
-
-                var bounds = {
-                    x: x.invert((0 - zoom.translate()[0]) / zoom.scale()),
-                    y: z.invert((view.screen.h - zoom.translate()[1]) / zoom.scale()),
-                    w: (x.invert(view.screen.w) - x.invert(0)) / zoom.scale(),
-                    h: (z.invert(0) - z.invert(view.screen.h)) / zoom.scale()
-                };
-
-                drawSystems(systemTree.search(bounds), scale);
-            };
-
-            function drawSystems(systems, scale) {
-
-                if(scale > 8) {
-                    var ss = map.selectAll('circle')
-                        .data(systems, function(s) {
-                            return s.id;
-                        });
-
-                    ss.enter()
-                        .append('circle')
-                        .attr('cx', function(d) { return x(d.x); })
-                        .attr('cy', function(d) { return z(d.z); })
-                        .attr('r', .15)
-                        .style('stroke-width', '0.05')
-                        .style('stroke', function(d) { return colors(d.regionId); })
-                        .style('fill', '#FFF');
-
-                    ss.exit()
-                        .remove();
-
+                // TODO: move this log into drawSystems / drawSystemLabels
+                if(map.zoom.scale() > 8) {
+                    drawSystems(map, systems);
                 } else {
-                    map.selectAll('circle').remove();
+                    clearSystems(map);
                 }
 
-                if(scale > 16) {
-                    var ss = map.selectAll('text')
-                        .data(systems, function(s) {
-                            return s.id;
-                        });
-
-                    ss.enter()
-                        .append('text')
-                        .attr('x', function(d) { return x(d.x); })
-                        .attr('y', function(d) { return z(d.z); })
-                        .attr('dy', '0.175em')
-                        .attr('dx', .5)
-                        .style({
-                            'font-family': 'helvetica, arial',
-                            'font-size': '0.5px'
-                        })
-                        .text(function(d) { return d.name; });
-
-                    ss.exit()
-                        .remove();
-
+                if(map.zoom.scale() > 16) {
+                    drawSystemLabels(map, systems);
                 } else {
-                    map.selectAll('text').remove();
+                    clearSystemLabels(map);
                 }
-            };
+            });
 
-            function zoomSystem(system) {
 
-                // var height = 13896860379771180;
-                var height = 30000000000000000;
-                var width = height * (view.screen.w / view.screen.h);
-
-                var scale = (z.invert(0) - z.invert(view.screen.h)) / height;
-
-                var transform = {
-                    scale: scale,
-                    x: -x(system.x - width / 2) * scale,
-                    y: view.screen.h - z(system.z - height / 2) * scale
-                };
-
-                d3.transition().duration(2000).tween("zoom", function() {
-
-                    var ix = d3.interpolate(zoom.translate()[0], transform.x),
-                        iy = d3.interpolate(zoom.translate()[1], transform.y)
-                        is = d3.interpolate(zoom.scale(), transform.scale);
-
-                    return function(t) {
-                        zoom.translate([ix(t), iy(t)]);
-                        zoom.scale(is(t));
-                        zoomed();
-                    };
-                });
-            }
-
-            zoomed();
-
+            // TODO: maybe have an init function or something that takes a map to wire shit up
             $($window).bind("resize", function(ev) {
-                view.screen = {
+                map.screen = {
                     h: $window.innerHeight,
                     w: $window.innerWidth
                 };
@@ -266,7 +337,7 @@ angular.module('evemaps').directive('evemap', ['$window', 'MapsService', functio
                 if(!mode) return;
 
                 if(mode.type === "zoom") {
-                    zoomSystem(mode.target);
+                    zoomSystem(map, mode.target);
                 }
             });
         }
